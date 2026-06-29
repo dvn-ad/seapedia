b908c27017d3db58f1d34f9ee3aab70bca11439e
dvn <davin.adisuryo@gmail.com>
2026-06-29 20:05:25 +0700

    feat: add godotenv for environment variable management and update frontend package name

--- backend/internal/config/db.go
+++ backend/internal/config/db.go
@@ -8,23 +8,25 @@ import (
 	"github.com/dvn-ad/seapedia/backend/internal/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
-func getEnv(key,fallback string)string{
-	if value,exists:=os.LookupEnv(key);exists{
-		return value
-	}
-	return fallback
+func getEnv(key string)string{
+	value,_:=os.LookupEnv(key)
+	return value
 }
 
 func InitDB(){
-	host:=getEnv("DB_HOST","localhost")
-	port:=getEnv("DB_PORT","5432")
-	user:=getEnv("DB_USER","postgres")
-	pass:=getEnv("DB_PASSWORD","postgres")
-	dbname:=getEnv("DB_NAME","seapedia")
+	if err:=godotenv.Load();err!=nil{
+		log.Fatalf("Couldnt find .env file")
+	}
+	host,_:=os.LookupEnv("DB_HOST")
+	port,_:=os.LookupEnv("DB_PORT")
+	user,_:=os.LookupEnv("DB_USER")
+	pass,_:=os.LookupEnv("DB_PASSWORD")
+	dbname,_:=os.LookupEnv("DB_NAME")
 	
 	dsn:=fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", host,user,pass,dbname,port)
 
